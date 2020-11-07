7e2dc140f4e9aca286094a0bef5407a2daf670d4
David <davidirvin47@gmail.com>
2020-11-07 11:39:32 -0700

    revert changes 2

--- app.js
+++ app.js
@@ -16,6 +16,13 @@ app.use(bodyParser.urlencoded({
 app.use(cors());
 app.listen(port,() => console.log("Backend started on port " + port));
 
+if (process.env.NODE_ENV === "production") {
+    console.log("production");
+    app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, 'client', "build", "index.html")));
+};
+
 const mongoURI = process.env.REACT_APP_MONGO_URI;
 
 mongoose.connect(
