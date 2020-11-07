e389ad31c88f3bce632ec7a0c714ff025fc6e08c
David <davidirvin47@gmail.com>
2020-11-07 11:02:21 -0700

    fix 3

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 5000;
 if (process.env.NODE_ENV !== 'production') {
     app.use(express.static("client/build"));
     app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, 'client', "build", "index.html")));
-    require('dotenv').config();
+    // require('dotenv').config();
 }
 
 app.use(bodyParser.urlencoded({
@@ -22,7 +22,7 @@ app.use(bodyParser.urlencoded({
 app.use(cors());
 app.listen(port,() => console.log("Backend started on port " + port));
 
-const mongoURI = process.env.REACT_APP_MONGO_URI;
+const mongoURI = "mongodb+srv://admin-david:test123@cluster0-6ghui.mongodb.net/dayplannerDB";
 
 mongoose.connect(
     mongoURI, 
