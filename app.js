d0c637c7a5334c1d4e0bec2e1bdf790496902960
David <davidirvin47@gmail.com>
2020-11-07 10:56:14 -0700

    fix

--- app.js
+++ app.js
@@ -2,12 +2,17 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config();
-}
+
+
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+if (process.env.NODE_ENV !== 'production') {
+    app.use(express.static("client/build"));
+    app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, 'client', "build", "index.html")));
+    require('dotenv').config();
+}
 
 app.use(bodyParser.urlencoded({
     extended: true
